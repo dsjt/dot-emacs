e212118d2d118365b23d8728315fc96ca1e536c0
Ishida Tatsuhiro <toot.daiylfalaiydt@gmail.com>
2016-07-02 21:27:16 +0900

    [comment out] hippie-expand config changed

--- init.el
+++ init.el
@@ -298,19 +298,17 @@
 ;; (add-hook 'scheme-mode-hook 'enable-paredit-mode)
 
 ;; expand
-;; (require 'hippie-exp)
-;; (eval-after-load "hippie-exp"
-;;   '(setq hippie-expand-try-functions-list
-;;          '(try-expand-all-abbrevs
-;;            try-expand-dabbrev
-;;            try-expand-dabbrev-all-buffers
-;;            try-expand-dabbrev-from-kill
-;;            try-complete-file-name-partially
-;;            try-complete-file-name
-;;            try-complete-lisp-symbol-partially
-;;            try-complete-lisp-symbol)))
 (global-set-key (kbd "C-;") 'hippie-expand)
-
+(setq hippie-expand-try-functions-list
+      '(try-complete-file-name-partially
+        try-complete-file-name
+        try-expand-all-abbrevs
+        try-expand-dabbrev
+        try-expand-dabbrev-all-buffers
+        try-expand-dabbrev-from-kill
+        try-complete-lisp-symbol-partially
+        try-complete-lisp-symbol))
+ 
 ;; scroll 
 (el-get-bundle 'yascroll)
 (global-yascroll-bar-mode 1)
