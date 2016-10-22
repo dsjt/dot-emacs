776bb469c2a77bf44524b6f17ac26ccc92306e55
dsjt <toot.daiylfalaiydt@gmail.com>
2016-10-22 12:08:28 +0900

    [add] clipmon

--- init.el
+++ init.el
@@ -935,10 +935,14 @@
 
 ;; 透明度を変更するコマンド M-x set-alpha
 ;; http://qiita.com/marcy@github/items/ba0d018a03381a964f24
+;;;###autoload
 (defun set-alpha (alpha-num)
   "set frame parameter 'alpha"
   (interactive "nAlpha: ")
   (set-frame-parameter nil 'alpha (cons alpha-num '(90))))
+
+;; clipmon
+(el-get-bundle bburns/clipmon)
 
 ;; private
 (load-file "~/.emacs.d/private.el")
